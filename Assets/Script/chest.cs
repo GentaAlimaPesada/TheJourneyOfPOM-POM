bd611a93348dbdec34d7cb7c9e31e6c6b871a0cf
Genta Alima Persada <gentaalimapersada@upi.edu>
2024-03-05 18:50:22 +0700

    fist commit

--- Assets/Script/chest.cs
+++ Assets/Script/chest.cs
@@ -1,78 +1,36 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class chest : MonoBehaviour
 {
-    private bool playerNearChest = false;
-    [SerializeField] private GameObject character;
-    [SerializeField] private GameObject panelVictory;
     [SerializeField] private GameObject informationText;
-    [SerializeField] private float displayTime = 2f; // Waktu tampilan teks setelah pemain menjauh
-
-    private Coroutine displayCoroutine;
-
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private GameObject panelVictory;
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            ShowInformationText();
-            playerNearChest=true;
+            informationText.SetActive(true);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            HideInformationTextAfterDelay();
-            playerNearChest=false; 
+            informationText.SetActive(false);
         }
     }
-
-    private void OnTriggerStay2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
                 panelVictory.SetActive(true);
-                character.gameObject.GetComponent<CharacterControler>().enabled = false;
-                character.gameObject.GetComponent<Animator>().enabled = false;
+                audioSource.Play();
             }
         }
-    }
-
-    private void Update()
-    {
-        if (playerNearChest == true)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                panelVictory.SetActive(true);
-                character.gameObject.GetComponent<CharacterControler>().enabled = false;
-                character.gameObject.GetComponent<Animator>().enabled = false;
-            }
-        }
-    }
-
-    private void ShowInformationText()
-    {
-        if (displayCoroutine != null)
-        {
-            StopCoroutine(displayCoroutine);
-        }
-
-        informationText.SetActive(true);
-    }
-
-    private void HideInformationTextAfterDelay()
-    {
-        displayCoroutine = StartCoroutine(HideInformationTextDelayed());
-    }
-
-    private IEnumerator HideInformationTextDelayed()
-    {
-        yield return new WaitForSeconds(displayTime);
-        informationText.SetActive(false);
     }
 }
